e61342a23723e188cd1752f08145b38e0fe0dfb3
maruf <maruf.cuet.cse16@gmail.com>
2025-05-02 14:29:57 +0600

    CertificateUpdate Callable class added

---
+++ src/main/java/com/solvians/showcase/CertificateUpdateCallable.java
@@ -0,0 +1,30 @@
+package com.solvians.showcase;
+
+import java.time.LocalDate;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class CertificateUpdateCallable implements Callable<> {
+    @Override
+    public String call() throws Exception {
+        ThreadLocalRandom random = ThreadLocalRandom.current();
+        // provided long
+        long timestamp =  System.currentTimeMillis();
+        String isIn = ISINGenerator.generateIsIn();
+
+        double bidPrice = random.nextDouble(100.00, 200.00);
+//        System.out.println("bidPrice : " + bidPrice);
+
+        int bidSize = random.nextInt(1000,5000);
+
+        double askPrice = random.nextDouble(100.00, 200.00);
+
+        int askSize = random.nextInt(1000,10000);
+
+        LocalDate maturityDate = LocalDate.now().plusYears(2);
+        String res = String.format("%d,%s,%.2f,%d,%.2f,%d",
+                timestamp,isIn,bidPrice,bidSize,askPrice,askSize);
+
+        return res;
+    }
+}
